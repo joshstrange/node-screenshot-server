f3f96acd927affcd7e24b2303bb8bb86af29dd85
Josh Strange <josh@joshstrange.com>
2015-03-17 15:55:10 -0400

    Adding support for hoverzoom

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ var userId = 2148004;
 var redirectRoot = 'http://joshstrange.com';
 
 app.set('view engine', 'ejs');
-app.set('views',__dirname);
+app.set('views', __dirname);
 
 app.get('/', function (req, res) {
 	res.redirect(redirectRoot);
 });
 
 app.get('/:image', function (req, res) {
-	res.render('index.ejs', {
-		imageUrl: 'https://dl.dropboxusercontent.com/u/' + userId + '/Screenshots/' + req.params.image + (req.params.image.indexOf('.png') === -1 ? '.png' : '')
-	});
-	//res.send('Hello World!')
+	var imageUrl = 'https://dl.dropboxusercontent.com/u/' + userId + '/Screenshots/' + req.params.image + (req.params.image.indexOf('.png') === -1 ? '.png' : '');
+	if(req.headers.accept.indexOf('image/webp') === 0) {
+		//If we are being asked for an image first and foremost send them to DB
+		res.redirect(imageUrl);
+	} else {
+		res.render('index.ejs', {
+			imageUrl: imageUrl
+		});
+	}
 });
 
 var server = app.listen(19910, function () {
